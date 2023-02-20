ccbcf487f1e951e471f6c56d2a0309c55f5536ec
Kevin Lau <Kevin.lau15@yahoo.com>
2023-02-20 17:17:05 -0500

    Updated Data.py
    Made it so all of the 6000+ courses data are in a python array

--- data.py
+++ data.py
@@ -1,24 +1,48 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 # Set up options to run Chrome headless
 options = Options()
-options.headless = True
+#options.add_argument('--headless')
 
 # Set up the webdriver
 driver = webdriver.Chrome(options=options)
 
 # Navigate to the webpage
-url = "https://uwflow.com/course/math136"
+url = "https://uwflow.com/explore"
 driver.get(url)
 
 # Wait for the page to finish loading (adjust the time.sleep duration as needed)
 import time
-time.sleep(5)
+time.sleep(1)
+
+# Scrolling down to the bottom of the page to get all 6000 courses
+while True:
+    count=count+1
+    # Get current scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    # Scroll down to the bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # Wait for new content to load
+    time.sleep(0.65)
+
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
 
 # Extract the title of the webpage
-title = driver.title
-print(title)
+titles = driver.find_elements(By.CLASS_NAME, 'sc-pQdCa')
+
+# Extract the title and rating of each card
+for title in titles:
+    print(title.text)
 
 # Quit the webdriver
 driver.quit()
