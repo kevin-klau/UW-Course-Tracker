c6e39b8d3f7dbe30cf7fa2029df482e64cfbfc1b
Kevin Lau <Kevin.lau15@yahoo.com>
2023-02-22 16:51:47 -0500

    Updated Website to Have Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
+import background from './Background-Bottom.png'
 
 function App() {
   return (
-    <div id="heading">
-      <h1 id="title">UW COURSE TRACKER<div id="stick-out"></div></h1>
-      <row>
-        <div class="col-lg-4"></div>
-        <h2 id="subtitles" class="col-lg-2"> alsdj</h2>
-      </row>
+    <div>
+      <div id="heading">
+        <h1 id="title">UW Course Tracker<div id="stick-out"></div></h1>
+      </div>
+      <div>
+        <img src={background} alt="Background" id="back-bottom"></img>
+        <h2 id="subtitles"> Designed By: Kevin Lau | Data Source: UWFlow </h2>
+      </div>
+      
+      <div class="bd-search">
+
+      </div>
+      
     </div>
+    
   );
 }
 
