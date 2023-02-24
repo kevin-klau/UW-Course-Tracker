3a369e44e3379119fbcaf11cf2304b94917b6e74
Kevin Lau <Kevin.lau15@yahoo.com>
2023-02-23 20:44:03 -0500

    Completed Search bar and deleted images

--- src/form.js
+++ src/form.js
@@ -1,22 +1,35 @@
 import { useState } from 'react';
 import { Courses } from './courselist.js';
+import { useEffect } from 'react';
+import Stats from './stats.js';
 import './App.css';
+import './button-colours.css';
 import Fuse from 'fuse.js';
 
-function CoolButton ({ object }){
+
+/*function CoolButton ({ prop }){
+    let classInfo = prop.faculty;
     return (
-        <button>object.</button>
+        <button id="SearchResults" class={classInfo+" btn btn-outline-success"} style={{borderWidth:"3px"}}>{prop.course}</button>
     );
-}
+}*/
+
+
 
 export default function Form(){
     
+    // Setting if the graphs and stuff should be revealed
+    const [revealInfo, setRevealInfo] = useState(false);
+
     // Setting User Input
     const [userInput, setUserInput] = useState('');
     
+    // Checking whether or not to reveal alert
+    const [alert, setAlert] = useState(false);
+
     // Setting up the fuzzy search
     const options = {
-        keys:['course'],
+        keys:['course', 'name'],
         includeScore:true,
         threshold: 1
     };
@@ -28,30 +41,58 @@ export default function Form(){
     // Function to include results
     function MostRelevant(e){
         e.preventDefault();
+        setAlert(false);
         setUserInput(e.target.value);
-        setResults(fuse.search(e.target.value).slice(0,5));
+        if (e.target.value !== ""){
+            setResults(fuse.search(e.target.value).slice(0,5));
+        }
     }
 
     // Function to handle when they want to search
     function Submitted(e){
         e.preventDefault();
-        //if ({userInput}.toLowerCase() !== )
+
+        if (userInput.toUpperCase() !== results[0].item.course){
+            setAlert(true);
+        }else{
+            setAlert(false);
+            Selected(results[0].item);
+        }
     }
     
-    return(
-        <form id="form" class="d-flex container" role="search" onSubmit={Submitted}>
-            <input onChange={MostRelevant} id="input" class="form-control me-2 d-flex" type="search" placeholder="Enter Subject Here" aria-label="Search"></input>
-            <button id="inputbutton" class="btn btn-outline-success" type="submit">🔎︎</button>
-            <ul>
-                
-                <button key={results[0].course}>{results[0].faculty}</button>
-                {results.map((result) => (
+    const [selectedCourse, setSelectedCourse] = useState({});
 
-                <button key={result.item.course}>{result.item.course}</button>
-                
+    // Function to handle when they finally choose a subject
+    function Selected({ prop }){
+        setRevealInfo(true);
+        setSelectedCourse ({prop});
+    }
+
+
+    
+    return(
+        <div>
+            <form id="form" class="d-flex container" role="search" onSubmit={Submitted} style={{position:'relative'}}>
+                <input onChange={MostRelevant} id="input" class="form-control me-2 d-flex clear" type="search" placeholder="Enter Subject Here" aria-label="Search"></input>
+                <button id="inputbutton" class="btn btn-outline-success" type="submit">🔎︎</button>
+                <div className={`fade ${alert ? 'show' : 'hide'}`} style={{ position: 'absolute', top: 5, left: -40, display:'flex', textAlign:'Center'}}>
+                        <div id="alert" class="alert alert-danger" role="alert">
+                            Invalid Course Subject
+                        </div>
+                </div>
+            </form>
+            <div class="container">
+                {results.map((result) => (
+                <button id="SearchResults" class={result.item.faculty+" btn btn-outline-success"} style={{borderWidth:"3px"}} onclick={() => Selected(result.item)}>{result.item.course}</button>
                 ))}
-            </ul>
-        </form>
+            </div>
+
+        <div>
+            {revealInfo ? <Stats props={selectedCourse}></Stats> : <div/>}
+        </div>
+            
+
+        </div>
 
     );
 
---
+++ src/stats.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import './App.css';
+
+export default function Stats({ props }){
+
+    return(
+        <div>
+        <p>hiAJDOIADJWIOJDAOIWJD</p>
+
+        <h1>{props.faculty}</h1>
+        </div>
+
+    );
+
+}
