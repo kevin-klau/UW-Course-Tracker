c211c31afc040a57e0d9f1b001309a88ae1612a3
Kevin Lau <Kevin.lau15@yahoo.com>
2023-02-25 17:43:25 -0500

    undid changes

--- src/stats.js
+++ src/stats.js
@@ -1,13 +1,78 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import data from './datafull.json';
+import { ResponsiveScatterPlot, ScatterPlot } from '@nivo/scatterplot';
+
+const MyResponsiveScatterPlot = ({ data /* see data tab */ }) => (
+    <ResponsiveScatterPlot
+        data={data}
+        margin={{ top: 60, right: 140, bottom: 70, left: 90 }}
+        xScale={{ type: 'linear', min: 0, max: 'auto' }}
+        xFormat=">-.2f"
+        yScale={{ type: 'linear', min: 0, max: 'auto' }}
+        yFormat=">-.2f"
+        blendMode="multiply"
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+            orient: 'bottom',
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'weight',
+            legendPosition: 'middle',
+            legendOffset: 46
+        }}
+        axisLeft={{
+            orient: 'left',
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'size',
+            legendPosition: 'middle',
+            legendOffset: -60
+        }}
+        legends={[
+            {
+                anchor: 'bottom-right',
+                direction: 'column',
+                justify: false,
+                translateX: 130,
+                translateY: 0,
+                itemWidth: 100,
+                itemHeight: 12,
+                itemsSpacing: 5,
+                itemDirection: 'left-to-right',
+                symbolSize: 12,
+                symbolShape: 'circle',
+                effects: [
+                    {
+                        on: 'hover',
+                        style: {
+                            itemOpacity: 1
+                        }
+                    }
+                ]
+            }
+        ]}
+    />
+)
 
 export default function Stats({ props }){
-    const [filteredInfo, setFilteredInfo] = useState(data.filter(item => item.code.substring(0, props.course.length+1) ===(props.course+" ")));
+    const [filteredInfo, setFilteredInfo] = useState([]);
+
+    useEffect(() => {
+        setFilteredInfo(data.filter(item => item.code.substring(0, props.course.length+1) ===(props.course+" ")));
+    }, [props]);
     
+    const scatterplotData = filteredInfo.map(item => ({
+        x: item.ratings,
+        y: item.useful
+    }));
 
     return(
-        <div>
+        <div id="statistics">
+            <MyResponsiveScatterPlot data={scatterplotData}/>
             <h1>{props.course}</h1>
             <h2>{props.name}</h2>
             {filteredInfo.map(item => (
@@ -26,16 +91,13 @@ export default function Stats({ props }){
 
 }
 
-/*//const lengthCourse = course.length;
-    //const [relevantCourses, setRelevantCourses] = useState([]);
-    //data.filter(courseCode => (courseCode).code.substring(0,lengthCourse) === (course).substring(0,lengthCourse))
-
-    return(
-        <div>
-            <h1>{course}</h1>
-            <h2>{name}</h2>
-            <h3>{faculty}</h3>
-            
-        </div>
-
-    );*/
+/*{filteredInfo.map(item => (
+                <div key={item.code}>
+                    <h2>{item.code}</h2>
+                    <p>{item.name}</p>
+                    <p>Ratings: {item.ratings}</p>
+                    <p>Useful: {item.useful}</p>
+                    <p>Easy: {item.easy}</p>
+                    <p>Liked: {item.liked}</p>
+                </div>
+            ))}*/
