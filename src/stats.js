d6615f65a348506c8d23c8143540db721e345572
Kevin Lau <Kevin.lau15@yahoo.com>
2023-02-26 05:26:54 -0500

    added charts

--- src/stats.js
+++ src/stats.js
@@ -1,90 +1,51 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import data from './datafull.json';
-import { ResponsiveScatterPlot, ScatterPlot } from '@nivo/scatterplot';
+import {ScatterChart, Scatter, XAxis, YAxis, ZAxis, ReferenceArea, ReferenceDot, ReferenceLine, Brush, Legend, CartesianGrid, Tooltip, ResponsiveContainer} from "recharts";
 
-const MyResponsiveScatterPlot = ({ data /* see data tab */ }) => (
-    <ResponsiveScatterPlot
-        data={data}
-        margin={{ top: 60, right: 140, bottom: 70, left: 90 }}
-        xScale={{ type: 'linear', min: 0, max: 'auto' }}
-        xFormat=">-.2f"
-        yScale={{ type: 'linear', min: 0, max: 'auto' }}
-        yFormat=">-.2f"
-        blendMode="multiply"
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-            orient: 'bottom',
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'weight',
-            legendPosition: 'middle',
-            legendOffset: 46
-        }}
-        axisLeft={{
-            orient: 'left',
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'size',
-            legendPosition: 'middle',
-            legendOffset: -60
-        }}
-        legends={[
-            {
-                anchor: 'bottom-right',
-                direction: 'column',
-                justify: false,
-                translateX: 130,
-                translateY: 0,
-                itemWidth: 100,
-                itemHeight: 12,
-                itemsSpacing: 5,
-                itemDirection: 'left-to-right',
-                symbolSize: 12,
-                symbolShape: 'circle',
-                effects: [
-                    {
-                        on: 'hover',
-                        style: {
-                            itemOpacity: 1
-                        }
-                    }
-                ]
-            }
-        ]}
-    />
-)
 
 export default function Stats({ props }){
     const [filteredInfo, setFilteredInfo] = useState([]);
+
+    const chartStyle={
+        fontFamily: 'Anderson Grotesk',
+        
+    };
 
     useEffect(() => {
         setFilteredInfo(data.filter(item => item.code.substring(0, props.course.length+1) ===(props.course+" ")));
     }, [props]);
     
-    const scatterplotData = filteredInfo.map(item => ({
-        x: item.ratings,
-        y: item.useful
+    const scatterPlotData = filteredInfo.map(item => (Number(item.ratings) >= 5 && {
+        name: item.name,
+        x: Number(item.easy.substring(0,item.easy.length-1)),
+        y: Number(item.useful.substring(0,item.easy.length-1)),
+        z: Number(item.ratings.substring(0,item.easy.length-1))
     }));
 
     return(
         <div id="statistics">
-            <MyResponsiveScatterPlot data={scatterplotData}/>
+            
             <h1>{props.course}</h1>
             <h2>{props.name}</h2>
-            {filteredInfo.map(item => (
-                <div key={item.code}>
-                    <h2>{item.code}</h2>
-                    <p>{item.name}</p>
-                    <p>Ratings: {item.ratings}</p>
-                    <p>Useful: {item.useful}</p>
-                    <p>Easy: {item.easy}</p>
-                    <p>Liked: {item.liked}</p>
-                </div>
-            ))}
+            <ScatterChart
+                width={600}
+                height={500}
+                margin={{
+                    top: 20,
+                    right: 20,
+                    bottom: 10,
+                    left: 10,
+                }}
+                >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="x" type="number" name="Easiness" unit="%" />
+                <YAxis dataKey="y" type="number" name="Usefulness" unit="%" />
+                <ZAxis dataKey="z" type="number" range={[64, 144]} name="Ratings" unit="" />
+                <Tooltip class="chart" cursor={{ strokeDasharray: '10 10'}} animationDuration={1000} animationEasing={'ease'} itemStyle={chartStyle} />
+                <Legend />
+                <Scatter name="A pool" data={scatterPlotData} fill="green" />
+                </ScatterChart>
         </div>
 
     );
